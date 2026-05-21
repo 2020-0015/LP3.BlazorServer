016864c1b8f877a1a47230b4f1275bf3beff43c0
2020-0015 <2020-0015@uteco.edu.do>
2026-05-20 20:18:52 -0400

    ...

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -5,4 +5,16 @@ namespace LP3.BlazorServer.Data;
 
 public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options)
 {
+    public DbSet<Estudiante> Estudiantes=>Set<Estudiante>(); 
+    
+    public DbSet<Curso> Cursos=>Set<Curso>();
+
+    public DbSet<Matriculacion> Matriculaciones=>Set<Matriculacion>();
+
+    public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+    {
+        return base.SaveChangesAsync(cancellationToken);
+    }
 }
+
+
